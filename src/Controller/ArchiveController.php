86719eafedc80aafc96650d2c3c00e6d30ce32d9
DSM892 <sassidayssem@gmail.com>
2026-09-23 17:54:04 +0200

    TP4 2/3 & TP4 3/3

--- src/Controller/ArchiveController.php
+++ src/Controller/ArchiveController.php
@@ -6,8 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+#[Route('/archives', name: 'archives_')]
 
 final class ArchiveController extends AbstractController
 {
-    
+    #[Route('/{annee}', name: 'annee', requirements: ['annee' => '\d{4}$'])]
+    public function archiveAnnee(int $annee): Response
+    {
+        $date = new \DateTimeImmutable("$annee-01-01");
+
+        return $this->render('archives/annee.html.twig', [
+            'title' => 'Archive Annee',
+            'archive_date' => $date,
+        ]);
+    }
+
+    #[Route('/apercu/{uuid}', name: 'apercu_uuid', requirements: ['uuid' => Requirement::UUID_V4])]
+    public function archiveApercuUUID(string $uuid): Response
+    {
+        return $this->render('archives/apercu/brouillon.html.twig', [
+            'title' => 'Archive Apercu UUID',
+        ]);
+    }
 }
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,9 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route('/article', name: 'article_')]
+
 final class ArticleController extends AbstractController
 {
-    #[Route('/article/{id}', name: 'article_id', requirements: ['id' => Requirement::DIGITS])]
+    #[Route('/{id}', name: 'id', requirements: ['id' => Requirement::DIGITS])]
     public function articleId(int $id): Response
     {
         return $this->render('article/show.html.twig', [
@@ -19,12 +21,49 @@ final class ArticleController extends AbstractController
         ]);
     }
 
-    #[Route('/article/slug/{slug}', name: 'article_slug', requirements: ['slug' => '[a-z0-9]+'])]
+    #[Route('/slug/{slug}', name: 'slug_slug', requirements: ['slug' => '[a-z0-9]+'])]
+    public function articleSlugSlug(string $slug): Response
+    {
+        return $this->render('article/slug/slug.html.twig', [
+            'title' => 'Article Slug',
+            'article_slug' => $slug,
+        ]);
+    }
+
+    #[Route('/nouveau', name: 'nouveau')]
+    public function articleNouveau(): Response
+    {
+        return $this->render('article/nouveau.html.twig', [
+            'title' => 'Article Nouveau',
+            'article_nouveau' => 'Nouvel article',
+        ]);
+    }
+
+    #[Route('/{tri}', name: 'tri', requirements: ['tri' => 'recents|anciens|commentes'])]
+    public function articleTri(string $tri): Response
+    {
+        return $this->render('article/tri.html.twig', [
+            'title' => 'Article Tri',
+            'type_tri' => $tri,
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'slug', requirements: ['slug' => '[a-z0-9]+'])]
     public function articleSlug(string $slug): Response
     {
         return $this->render('article/slug.html.twig', [
             'title' => 'Article Slug',
             'article_slug' => $slug,
+        ]);
+    }
+
+    #[Route('/{slug}/partage', name: 'slug_partage', requirements: ['slug' => '[a-z0-9]+'])]
+    public function articleSlugPartage(string $slug): Response
+    {
+        return $this->render('article/slug/slug-partage.html.twig', [
+            'title' => 'Article Slug Partage',
+            'article_slug' => $slug,
+            'source' => "sourcePartage",
         ]);
     }
 }
